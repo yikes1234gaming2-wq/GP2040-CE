d5c813060ad0011987f5909fac93d96f43ff3617
yikes1234gaming2-wq <yikes1234gaming2@gmail.com>
2026-07-31 19:09:27 -0600

    Create mpr121.h

---
+++ headers/addons/mpr121.h
@@ -0,0 +1,19 @@
+#ifndef _MPR121_H_
+#define _MPR121_H_
+
+#include "gpaddon.h"
+#include "GamepadEnums.h"
+
+#ifndef MPR121_I2C_ADDR
+#define MPR121_I2C_ADDR 0x5A
+#endif
+
+class MPR121Input : public GPAddon {
+public:
+    virtual bool available() override;
+    virtual void setup() override;
+    virtual void process() override;
+    virtual std::string name() override { return "MPR121"; }
+};
+
+#endif        // _MPR121_H_
