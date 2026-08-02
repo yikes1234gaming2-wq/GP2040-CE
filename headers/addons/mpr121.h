f705bd47b8ad12a8251b320fe1192576a2e16205
yikes1234gaming2-wq <yikes1234gaming2@gmail.com>
2026-08-01 20:42:18 -0600

    Update mpr121.h

--- headers/addons/mpr121.h
+++ headers/addons/mpr121.h
@@ -10,10 +10,10 @@
 
 class MPR121Input : public GPAddon {
 public:
-    virtual bool available() override;
+    virtual bool available() override { return true; }
     virtual void setup() override;
     virtual void process() override;
     virtual std::string name() override { return "MPR121"; }
 };
 
-#endif        // _MPR121_H_
+#endif // _MPR121_H_
