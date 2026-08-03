f685a409b745fa978f0e0ef02b189074580d44ed
yikes1234gaming2-wq <yikes1234gaming2@gmail.com>
2026-08-02 21:08:39 -0600

    Update mpr121.h

--- headers/addons/mpr121.h
+++ headers/addons/mpr121.h
@@ -2,7 +2,6 @@
 #define _MPR121_H_
 
 #include "gpaddon.h"
-#include "GamepadEnums.h"
 
 #ifndef MPR121_I2C_ADDR
 #define MPR121_I2C_ADDR 0x5A
@@ -10,13 +9,11 @@
 
 class MPR121Input : public GPAddon {
 public:
-    // Core GPAddon virtual methods
     virtual bool available() override { return true; }
     virtual void setup() override;
     virtual void process() override;
     virtual std::string name() override { return "MPR121"; }
 
-    // Additional GPAddon / GPPeripheral lifecycle hooks
     virtual void preprocess() override {}
     virtual void postprocess(bool processed) override {}
     virtual void reinit() override {}
